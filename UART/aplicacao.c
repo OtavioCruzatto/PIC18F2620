f2ecce2bbef3f6417eabeef7d1afa301757f82cc
Otavio Cruzatto <30843450+OtavioCruzatto@users.noreply.github.com>
2018-01-05 11:26:41 -0200

    Add files via upload

--- UART/aplicacao.c
+++ UART/aplicacao.c
@@ -5,21 +5,26 @@
  * Created on 29 de Dezembro de 2017, 10:11
  */
 
-
 #include <xc.h>
 #include <pic18f2620.h>
 #include "configurationBits.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "uart.h"
+#include <string.h>
+#include <ctype.h>
 
 void configureGpio();
+void tratarComando(char comando[]);
+
+char contadorBuffer = 0;
 
 void main(void) {
     
     configureUart();
     configureGpio();
     
+    escreverStringUart("Inicio...\n\n");
     while(1);
     
 }
@@ -29,23 +34,47 @@ void configureGpio() {
     PORTAbits.RA0 = 0;
 }
 
+void tratarComando(char comando[]) {
+    
+    if((strcmp(comando, "LIGAR")) == 0) {
+        escreverStringUart("Led ligado!\n\n");
+        PORTAbits.RA0 = 1;
+    }
+    else if((strcmp(comando, "DESLIGAR")) == 0) {
+        escreverStringUart("Led desligado!\n\n");
+        PORTAbits.RA0 = 0;
+    }
+    else {
+        escreverStringUart("Comando invalido!\n\n");
+    }
+    
+}
+
 /**
  * Função para tratamento das interrupções
  */
 void interrupt tratamento() {
     
+    char bufferSerial[50];
+    
     if(PIE1bits.RCIE && PIR1bits.RCIF) {
         
-        escreverCharUart(RCREG);
-        escreverStringUart("\n");
-        
-        if(RCREG == '0') {
-            escreverStringUart("Led desligado!\n\n");
-            PORTAbits.RA0 = 0;
+        if(RCREG != 13) {
+            escreverCharUart(RCREG);
+            bufferSerial[contadorBuffer] = toupper(RCREG);
+            contadorBuffer++;
+            if(contadorBuffer == 50) {
+                limparBuffer(bufferSerial);
+                contadorBuffer = 0;
+                escreverStringUart("\n\n\n!!! ESTOURO DE BUFFER !!!\n\n\n");
+            }
         }
-        else if(RCREG == '1') {
-            escreverStringUart("Led ligado!\n\n");
-            PORTAbits.RA0 = 1;
+        else if(RCREG == 13) {
+            escreverStringUart("\n");
+            verificarBuffer(bufferSerial,contadorBuffer);
+            contadorBuffer = 0;
+            tratarComando(bufferSerial);
+            limparBuffer(bufferSerial); 
         }
         
         PIR1bits.RCIF = 0;
